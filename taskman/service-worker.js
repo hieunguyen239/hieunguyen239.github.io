3964edcc7def31ef01ad2807b96c4fb7e8b87578
Hieu Nguyen FE <hieunm@forixwebdesign.com>
2019-02-07 21:36:13 +0700

    fix homepage

--- taskman/service-worker.js
+++ taskman/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c7570a9bc5084edc2d973abfa0a6a86.js"
+  "/taskman/precache-manifest.3b912563c3ed6a3842e34701cea488f5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/taskman/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
