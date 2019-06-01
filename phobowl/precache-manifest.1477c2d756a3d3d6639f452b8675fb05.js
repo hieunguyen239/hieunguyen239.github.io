f43b8a55e1f3bd919a624bf3afd742a66e5551f4
Hieu Nguyen FE <hieunm@forixwebdesign.com>
2019-06-01 18:02:31 +0700

    phobowl

---
+++ phobowl/precache-manifest.1477c2d756a3d3d6639f452b8675fb05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14eda3437eaae710af2b5c1720e0175",
+    "url": "/phobowl/index.html"
+  },
+  {
+    "revision": "0ee6a82bbf2485da7577",
+    "url": "/phobowl/static/css/main.5f79d700.chunk.css"
+  },
+  {
+    "revision": "e22d2d84ce47acadc3a6",
+    "url": "/phobowl/static/js/2.796b3fee.chunk.js"
+  },
+  {
+    "revision": "0ee6a82bbf2485da7577",
+    "url": "/phobowl/static/js/main.55431135.chunk.js"
+  },
+  {
+    "revision": "eec62490065f5d6cd3ff",
+    "url": "/phobowl/static/js/runtime~main.8292fe68.js"
+  }
+]);
